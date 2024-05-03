64b024b96f12671efc00fdd85797696dae3e716a
Nguyen Quang Linh <nguyenlinhak4@gmail.com>
2024-05-03 22:16:23 +0700

    Add project files.

---
+++ Book_Movie_Tickets/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Book_Movie_Tickets.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Book_Movie_Tickets.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
